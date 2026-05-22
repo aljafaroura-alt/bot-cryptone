a191b15c2957db4d1d210cfd65bb7300e04f492f
aljafaroura-alt <aljafaroura@gmail.com>
2026-05-22 19:59:30 +0700

    Fix formatting in bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ import schedule
 15  TOKEN = os.environ.get('TOKEN')
 16  bot = telebot.TeleBot(TOKEN)
 17  info = Info(constants.MAINNET_API_URL)
-18  
+18   
 19  # GLOBAL SWITCH
 20  SNIPER_ALL_COIN = False  # Default mati
 21  USER_ID = 8347576377  # PASTIIN ID TELEGRAM LU UDAH ADA
