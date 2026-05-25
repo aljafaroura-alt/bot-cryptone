1121be877f5f31e5763166a0e06a4e28bcc7169f
aljafaroura-alt <aljafaroura@gmail.com>
2026-05-25 17:44:50 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -2225,7 +2225,7 @@ def run_temen_scan(chat_id):
     try:
         data = info.meta_and_asset_ctxs()
         now = time.time()
-        alerts = [:3]
+        alerts = alerts[:3]   #SETTING-----------------
 
         for asset, ctx in zip(data[0]["universe"], data[1]):
             try:
